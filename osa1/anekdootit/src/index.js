ba81d765eacde58b9a627e68367064812199aa92
Joni Kopra <joni.kopra@gmail.com>
2020-09-18 18:39:02 +0300

    Fix Osa1

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -1,92 +1,49 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = ({ onClick, caption}) => {
-  return (
-    <>
-      <button onClick={onClick}>{caption}</button>
-    </>
-  )
-}
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(Array(props.anecdotes.length).fill(0))
+  const [most, setMost] = useState(0)
 
-const Statistics = ({good, neutral, bad}) => {
-  const all = () => (good + neutral + bad)
-
-  const average = () => {
-    return (
-      (good - bad) / all()
-    )
-  }
-  
-  const positive = () => {
-    return (
-      (good / all()) * 100
-    )
+  const nextAnecdote = () => {
+    setSelected(Math.floor(Math.random()*props.anecdotes.length))
   }
 
-  if (!all()) {
-    return <div>
-      <>No feedback given</>
-    </div>
-  }
-  return (
-    <div>
-      <table>
-        <tbody>
-          <StatisticLine text={'good'} value={good} />
-          <StatisticLine text={'neutral'} value={neutral} />
-          <StatisticLine text={'bad'} value={bad} />
-          <StatisticLine text={'average'} value={average()} />
-          <StatisticLine text={'positive'} value={positive() + ' %'} />
-        </tbody>
-      </table>
-    </div>
-  )
-}
+  const voteAnecdote = () => {
+    const copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
 
-const StatisticLine = (props) => {
-  return (
-    <tr>
-      <td>{props.text}</td>
-      <td>{props.value}</td>
-    </tr>
-  )
-}
-
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const handleGood = () => {
-    setGood(good + 1)
-  }
-
-  const handleNeutral = () => {
-    setNeutral(neutral + 1)
-  }
-
-  const handleBad = () => {
-    setBad(bad + 1)
+    if (copy[selected] > copy[most]) {
+      setMost(selected)
+    }
   }
 
   return (
     <div>
-      <h1>
-        give feedback
-      </h1>
-
-      <Button onClick={handleGood} caption='good' />
-      <Button onClick={handleNeutral} caption='neutral' />
-      <Button onClick={handleBad} caption='bad' />
-      
-      <h1>
-        statistics
-      </h1>
-
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <h1>Anecdote of the day</h1>
+      <div>{props.anecdotes[selected]}</div>
+      <div>has {points[selected]} votes</div>
+      <div>
+        <button onClick={voteAnecdote}>vote</button>
+        <button onClick={nextAnecdote}>next anecdote</button>
+      </div>
+      <h1>Anecdote with most votes</h1>
+      <div>{props.anecdotes[most]}</div>
+      <div>has {points[most]} votes</div>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Tämä on testianekdootti',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'))
